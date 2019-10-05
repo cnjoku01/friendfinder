71806ec6ec0be6f1e755e3dacfa372bd7befcd29
Chi Njoku <chin.njoku@gmail.com>
2019-10-05 18:20:21 -0500

    updating for incomplete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
-		name: "Mike",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		name: "Sasha",
+		photo: "https://pixabay.com/images/id-3064633/",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "Ashley",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		photo: "https://pixabay.com/images/id-3104635/",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Corey",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		name: "Will",
+		photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwinjZXMkIblAhVRHqwKHfcpDoEQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.cnn.com%2Fvideos%2Fmedia%2F2019%2F10%2F02%2Fwill-smith-fresh-prince-bel-air-clothing-line-mxp-vpx.hln&psig=AOvVaw3pUFtr0HiiqTDXMAwu28YT&ust=1570399605664702",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Kruz",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		name: "Marting",
+		photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj1w9fhkIblAhUKi6wKHR53CWUQjRx6BAgBEAQ&url=https%3A%2F%2Fpeople.com%2Fmovies%2Fwill-smith-martin-lawrence-say-bad-boys-3-happening%2F&psig=AOvVaw1IWYdEAQLT06X6xxsAYxVt&ust=1570399655431716",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Everton",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		name: "Jamie",
+		photo: "https://images.app.goo.gl/Ccej27Devqs1PwaKA",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
